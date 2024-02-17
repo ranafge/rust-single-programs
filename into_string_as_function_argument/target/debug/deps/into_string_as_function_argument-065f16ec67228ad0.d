853c40f80639a679670f25b76c767643eb7e18c0
ranafge <ranafge@gmail.com>
2024-02-17 11:33:42 +0600

    rust simple programming

---
+++ into_string_as_function_argument/target/debug/deps/into_string_as_function_argument-065f16ec67228ad0.d
@@ -0,0 +1,5 @@
+C:\Users\Rana\Documents\rust_sample_programs\into_string_as_function_argument\target\debug\deps\libinto_string_as_function_argument-065f16ec67228ad0.rmeta: src\main.rs
+
+C:\Users\Rana\Documents\rust_sample_programs\into_string_as_function_argument\target\debug\deps\into_string_as_function_argument-065f16ec67228ad0.d: src\main.rs
+
+src\main.rs:
---
+++ into_string_as_function_argument/target/debug/deps/into_string_as_function_argument-e86a2f8ee381b54e.d
@@ -0,0 +1,5 @@
+C:\Users\Rana\Documents\rust_sample_programs\into_string_as_function_argument\target\debug\deps\libinto_string_as_function_argument-e86a2f8ee381b54e.rmeta: src\main.rs
+
+C:\Users\Rana\Documents\rust_sample_programs\into_string_as_function_argument\target\debug\deps\into_string_as_function_argument-e86a2f8ee381b54e.d: src\main.rs
+
+src\main.rs:
